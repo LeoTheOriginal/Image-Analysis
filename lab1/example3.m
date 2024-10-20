a2d3efd447a3502bf127a71389013abc5eb02121
Dawid <dpiotrowski393939@gmail.com>
2024-10-20 21:50:06 +0200

    adding some essential

--- lab1/example3.m
+++ lab1/example3.m
@@ -1,12 +1,9 @@
 clear;clc;close all;
 im = imread('zubr.jpg');
 im=double(im)/255;
-%imshow(im);
-%figure;
-%imshow(im);
 h=3;
 w=2;
-
+%Organizacja RGB
 for i=1:3
     subplot(3,2, 2*i - 1);
     imshow(im(:,:,i));
